b42353454dbe2fb08c71081610b03210069ff5e7
ariyanreja27 <ariyanreja27@gmail.com>
2026-05-10 21:44:19 +0530

    chore: manual tanstack start configuration and removal of lovable assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
-    react(),
+    tanstackStart({
+      server: {
+        entry: "src/server.ts",
+      },
+    }),
     tailwindcss(),
     tsconfigPaths(),
   ],
