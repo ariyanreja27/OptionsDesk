b03dfe5661ba1d9084631ac6ea40f6756ea5c5ec
ariyanreja27 <ariyanreja27@gmail.com>
2026-05-11 13:57:45 +0530

    chore: add index.html and update cloudflare config to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
+      deployment: "cloudflare-pages",
       server: {
         entry: "src/server.ts",
       },
     }),
+    cloudflare(),
     tailwindcss(),
     tsconfigPaths(),
   ],
